e9aabc8c07a14ac8023871600175311e6a0490cc
Joy Lee <AmyLee521@outlook.com>
2024-10-30 08:56:06 +0000

    PF_CAM的heads从4改成6做训练和测试（效果变差）

--- test.sh
+++ test.sh
@@ -1,3 +1,1 @@
-python -W ignore test.py \
---load='/hexp/ly/PF_CAM/snapshot/sortnet/Net_epoch_best_pf_cam_squeeze.pth' \
-| tee "/hexp/ly/PF_CAM/log/print_out/test_pf_cam_squeeze.txt"
+python -W ignore test.py --load='/hexp/ly/PF_CAM/snapshot/sortnet/Net_epoch_best_pf_cam_h6_squeeze.pth' | tee "/hexp/ly/PF_CAM/log/print_out/test_pf_cam_h6_squeeze.txt"
