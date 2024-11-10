1643436f6b07c825f126202e1f4215df502be00f
Joy Lee <AmyLee521@outlook.com>
2024-11-10 02:32:46 +0000

    Revert "4pf_6_CAM_squeeze"
    This reverts commit b1658cfb166ca2bfcffe8463a0b93daa84800012.

--- networks/pf_cam.py
+++ networks/pf_cam.py
@@ -2,15 +2,14 @@ import torch
 from torch import nn
 from networks.resnet import resnet50
 from networks.srm_conv import SRMConv2d_simple
-from networks.cam import CAM
-# from networks.x_attn import CrossAttention
+from networks.x_attn import CrossAttention
 import torch.nn.functional as F
 
 
 class PF_CAM(nn.Module):
     def __init__(self, img_size, vit_patch_size, part_out, depth_self, depth_cross, n_heads=4, mlp_ratio=4., qkv_bias=True, p=0., attn_p=0., pretrain=True):
         super().__init__()
-        self.cam = CAM(img_size, vit_patch_size, part_out, depth_self, depth_cross, n_heads, mlp_ratio, qkv_bias, p, attn_p)
+        self.cam = CrossAttention(num_channels=3, num_heads=1)
         self.srm = SRMConv2d_simple()
         self.disc = resnet50(pretrained=True)
         self.disc.fc = nn.Linear(2048, 1)
