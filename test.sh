75f1bd070bd30aad38628fa0cd84a14f19e1677a
Joy Lee <AmyLee521@outlook.com>
2024-11-13 06:31:25 +0000

    4pfb_cam13_res6_se_squeeze test

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 python -W ignore test.py \
---load='/hexp/ly/PF_CAM/snapshot/sortnet/Net_epoch_best_4pfb_cam_res6_se_squeeze.pth' \
-| tee "/hexp/ly/PF_CAM/log/print_out/test_4pfb_cam_res6_se_squeeze.txt"
+--load='/hexp/ly/PF_CAM/snapshot/sortnet/Net_epoch_best_4pfb_cam13_res6_se_squeeze.pth' \
+| tee "/hexp/ly/PF_CAM/log/print_out/test_4pfb_cam13_res6_se_squeeze.txt"
